060929fd59e12ce00b9e4f4bec059693035a828b
o-tao <dhtkdals369@gmail.com>
2024-10-01 18:50:31 +0900

    [#1] TokenResponse 만료시간 추가

--- src/main/java/warriordiningback/token/response/TokenResponse.java
+++ src/main/java/warriordiningback/token/response/TokenResponse.java
@@ -1,12 +1,23 @@
 package warriordiningback.token.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.Date;
 
 @Getter
 @Builder
 public class TokenResponse {
     private String grantType;
+
     private String accessToken;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date accessTokenExpireAt;
+
     private String refreshToken;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date refreshTokenExpireAt;
 }
