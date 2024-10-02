fb76eba20d4a629216fdb1ea09d340c215b73553
o-tao <dhtkdals369@gmail.com>
2024-10-02 17:21:33 +0900

    [#1] 회원가입 API 추가

--- src/main/java/warriordiningback/api/user/controller/UserController.java
+++ src/main/java/warriordiningback/api/user/controller/UserController.java
@@ -1,12 +1,16 @@
 package warriordiningback.api.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import warriordiningback.api.dto.user.UserRequest;
+import warriordiningback.api.user.dto.SignInRequest;
+import warriordiningback.api.user.dto.UserRequest;
+import warriordiningback.api.user.dto.UserResponse;
 import warriordiningback.api.user.service.UserService;
+import warriordiningback.domain.user.User;
 import warriordiningback.token.response.TokenResponse;
 
 @RestController
@@ -17,8 +21,20 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signin")
-    public TokenResponse signIn(@RequestBody UserRequest userRequest) {
-        return userService.signIn(userRequest);
+    public TokenResponse signIn(@RequestBody @Valid SignInRequest signInRequest) {
+        return userService.signIn(signInRequest);
+    }
+
+    @PostMapping("/signup")
+    public UserResponse signUp(@RequestBody @Valid UserRequest userRequest) {
+        User user = userService.signUp(
+                userRequest.getEmail(),
+                userRequest.getPassword(),
+                userRequest.getName(),
+                userRequest.getBirth(),
+                userRequest.getPhone(),
+                userRequest.getGender());
+        return UserResponse.of(user);
     }
 
     @PostMapping("/test")
--- src/main/java/warriordiningback/api/user/service/UserService.java
+++ src/main/java/warriordiningback/api/user/service/UserService.java
@@ -1,44 +1,61 @@
 package warriordiningback.api.user.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import warriordiningback.api.dto.user.UserRequest;
+import warriordiningback.api.user.dto.SignInRequest;
+import warriordiningback.domain.Code;
+import warriordiningback.domain.CodeRepository;
+import warriordiningback.domain.user.Role;
+import warriordiningback.domain.user.RoleRepository;
+import warriordiningback.domain.user.User;
+import warriordiningback.domain.user.UserRepository;
 import warriordiningback.token.TokenProvider;
 import warriordiningback.token.response.TokenResponse;
 import warriordiningback.token.service.CustomUserDetailsService;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
 
+    private final UserRepository userRepository;
+
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final TokenProvider tokenProvider;
     private final CustomUserDetailsService customUserDetailsService;
+    private final CodeRepository codeRepository;
+    private final RoleRepository roleRepository;
 
-    public TokenResponse signIn(UserRequest userRequest) {
+    public TokenResponse signIn(SignInRequest signInRequest) {
         // 1. email + password를 기반으로 Authentication 객체 생성
         // 이때 authentication은 인증 여부를 확인하는 authenticated 값이 false
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(userRequest.getEmail());
-        log.info("user : {}", userDetails);
-
-
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(signInRequest.getEmail());
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                userRequest.getEmail(), userRequest.getPassword());
+                signInRequest.getEmail(), signInRequest.getPassword());
 
         // 2. 실제 검증: authenticate() 메서드를 통해 요청된 User에 대한 검증 진행
         // authenticate 메서드가 실행될 때 CustomUserDetailsService에서 만든 loadUserByUsername 메서드 실행
         Authentication authentication = authenticationManagerBuilder
                 .getObject().authenticate(authenticationToken);
-
+        
         // 3. 인증 정보를 기반으로 Jwt 토큰 생성
         return tokenProvider.generateToken(authentication);
     }
+
+    @Transactional
+    public User signUp(String email, String password, String name, String birth, String phone, Long gender) {
+        Code code = codeRepository.findById(gender).orElseThrow(() -> new RuntimeException("성별에 대한 정보가 존재하지않습니다."));
+        User savedUser = User.create(email, password, name, birth, phone, code);
+
+        Role defaultRole = roleRepository.findById(1L).orElseThrow(() -> new RuntimeException("권한에 대한 정보가 존재하지않습니다."));
+        savedUser.getRoles().add(defaultRole);
+
+        return userRepository.save(savedUser);
+    }
+
 }
