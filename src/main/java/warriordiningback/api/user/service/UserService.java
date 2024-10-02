40127c34bcf950713cd39b93570942f9ce287b5d
o-tao <dhtkdals369@gmail.com>
2024-10-02 17:32:44 +0900

    [#1] UserService 불필요 코드 제거

--- src/main/java/warriordiningback/api/user/service/UserService.java
+++ src/main/java/warriordiningback/api/user/service/UserService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import warriordiningback.api.user.dto.SignInRequest;
@@ -34,7 +33,6 @@ public class UserService {
     public TokenResponse signIn(SignInRequest signInRequest) {
         // 1. email + password를 기반으로 Authentication 객체 생성
         // 이때 authentication은 인증 여부를 확인하는 authenticated 값이 false
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(signInRequest.getEmail());
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 signInRequest.getEmail(), signInRequest.getPassword());
 
@@ -42,7 +40,7 @@ public class UserService {
         // authenticate 메서드가 실행될 때 CustomUserDetailsService에서 만든 loadUserByUsername 메서드 실행
         Authentication authentication = authenticationManagerBuilder
                 .getObject().authenticate(authenticationToken);
-        
+
         // 3. 인증 정보를 기반으로 Jwt 토큰 생성
         return tokenProvider.generateToken(authentication);
     }
