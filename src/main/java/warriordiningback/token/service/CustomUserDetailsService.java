690246f9558bdae019ca9a83894dc096a835a01a
o-tao <dhtkdals369@gmail.com>
2024-10-04 17:11:26 +0900

    [#3] 불필요 명령어 제거

--- src/main/java/warriordiningback/token/service/CustomUserDetailsService.java
+++ src/main/java/warriordiningback/token/service/CustomUserDetailsService.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import warriordiningback.domain.user.User;
 import warriordiningback.domain.user.UserRepository;
@@ -21,7 +20,6 @@ import java.util.stream.Collectors;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
