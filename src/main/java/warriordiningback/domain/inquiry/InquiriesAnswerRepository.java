1789ba12cd704bc7687729bbaeb3cad98c819a2b
mirinae <enkim0401@naver.com>
2024-10-11 15:17:09 +0900

    [#24] 문의사항 답변 빠진부분

---
+++ src/main/java/warriordiningback/domain/inquiry/InquiriesAnswerRepository.java
@@ -0,0 +1,9 @@
+package warriordiningback.domain.inquiry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquiriesAnswerRepository extends JpaRepository<InquiriesAnswer, Long>{
+	
+
+}
+//
