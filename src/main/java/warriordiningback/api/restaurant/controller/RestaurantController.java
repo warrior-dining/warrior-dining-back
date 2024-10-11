88cf399bcf1114522c11355cec7e5d531ec04e77
myin98 <dnjsdydpf@gmail.com>
2024-10-11 18:13:53 +0900

    [#22] 사용자 예약 top, 이달의 맛집 데이터 출력

--- src/main/java/warriordiningback/api/restaurant/controller/RestaurantController.java
+++ src/main/java/warriordiningback/api/restaurant/controller/RestaurantController.java
@@ -6,10 +6,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import warriordiningback.api.restaurant.dto.MonthReservationData;
+import warriordiningback.api.restaurant.dto.ReviewData;
+import warriordiningback.api.restaurant.dto.TopReservationData;
 import warriordiningback.api.restaurant.service.RestaurantService;
 import warriordiningback.domain.place.PlaceInfo;
 import warriordiningback.domain.place.PlaceRepository;
+
+import java.util.List;
 
 
 @RestController
@@ -24,7 +30,25 @@ public class RestaurantController {
 
     @GetMapping
     public Page<PlaceInfo> restaurantList(@PageableDefault(size = 10) Pageable pageable) {
-        return restaurantService.restaurantList(pageable);
+        return placeRepository.findAllBy(pageable, PlaceInfo.class);
+    }
+
+    @GetMapping("/top")
+    public List<TopReservationData> getTopReservations() {
+        return restaurantService.findTop10Places();
+    }
+
+    @GetMapping("/month")
+    public List<MonthReservationData> getMonthReservations(
+            @RequestParam(value = "year", required = false) Integer year,
+            @RequestParam(value = "month", required = false) Integer month) {
+
+        return restaurantService.findMonthPlaces(year, month);
+    }
+
+    @GetMapping("reviews")
+    public List<ReviewData> restaurantReviews() {
+        return restaurantService.findByReview();
     }
 
 
