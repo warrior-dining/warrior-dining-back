c591acfb7591d561b3b7868a25a4ce3c91c460f9
mirinae <enkim0401@naver.com>
2024-10-14 17:28:48 +0900

    [#36]관리자 예약관리 상태값 필터링 및 검색 -상태값으로 리스트 select하기 -검색기능

--- src/main/java/warriordiningback/api/admin/service/AdminReservationServiceImp.java
+++ src/main/java/warriordiningback/api/admin/service/AdminReservationServiceImp.java
@@ -5,17 +5,17 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.extern.slf4j.Slf4j;
 import warriordiningback.domain.Code;
 import warriordiningback.domain.CodeRepository;
 import warriordiningback.domain.reservation.Reservation;
 import warriordiningback.domain.reservation.ReservationRepository;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 public class AdminReservationServiceImp implements AdminReservationService{
@@ -30,13 +30,38 @@ public class AdminReservationServiceImp implements AdminReservationService{
 	public Map<String, Object> reservationList(String searchType, String searchKeyword, String status, Pageable pageable) {
 		Map<String, Object> resultMap = new HashMap<>();
 		Page<Reservation> searchReservation;
+		resultMap.put("status", false);
 		if(status != null && !status.isEmpty()) {
 			Code code = codeRepository.findById(Long.parseLong(status)).orElseThrow(()-> new RuntimeException("해당코드가 존재하지 않음"));
 			searchReservation = reservationRepository.findAllByCodeOrderByCreatedAtDesc(pageable, code);
-		} else {
-			searchReservation = reservationRepository.findAllByOrderByIdDesc(pageable);
+			resultMap.put("status", true);
+		} else if(searchKeyword != null && !searchKeyword.isEmpty()){
+			switch (searchType) {
+			case "id":
+				log.info("2");
+				searchReservation = reservationRepository.findById(Long.parseLong(searchKeyword), pageable);
+				break;
+			case "name":
+				searchReservation = reservationRepository.findAllByUserName(searchKeyword, pageable);
+				log.info("3");
+				break;
+			case "place":
+				searchReservation = reservationRepository.findAllByPlaceName(searchKeyword, pageable);
+				log.info("4");
+				break;
+			default:
+				searchReservation = reservationRepository.findAllByOrderByCreatedAtDesc(pageable);
+				log.info("5");
+				break;
+			}
+			resultMap.put("status", true);
+		}else {
+			searchReservation = reservationRepository.findAllByOrderByCreatedAtDesc(pageable);
+			log.info("6");
+			resultMap.put("status", true);
 		}
-		resultMap.put("data",searchReservation);
+		resultMap.put("status", true);
+		resultMap.put("results", searchReservation);
 		return resultMap;
 	}
 
