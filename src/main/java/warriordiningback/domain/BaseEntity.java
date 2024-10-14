e7d3bd5920a0ceba41958794248f7d9c3e2ccc5d
JJAJJAKIM <csm9121@gmail.com>
2024-10-14 15:52:01 +0900

    [#34] BaseEntity - createdAt, updatedAt에 jsonFormat 추가

--- src/main/java/warriordiningback/domain/BaseEntity.java
+++ src/main/java/warriordiningback/domain/BaseEntity.java
@@ -1,5 +1,6 @@
 package warriordiningback.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,9 +18,11 @@ public class BaseEntity {
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
