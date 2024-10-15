28f2e1072b3166e7c52ae5b20872ccdc2b8ebc27
myin98 <dnjsdydpf@gmail.com>
2024-10-15 12:40:50 +0900

    [#42] 사용자 음식점 detail 불러오는 레포지토리 추가

--- src/main/java/warriordiningback/domain/place/PlaceRepository.java
+++ src/main/java/warriordiningback/domain/place/PlaceRepository.java
@@ -11,9 +11,13 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
     <T> Page<T> findAllBy(Pageable pageable, Class<T> type); // 제네릭 메서드
 
     Page<Place> findAllByOrderByNameAsc(Pageable pageable);
+
     Page<Place> findByNameContainingOrderByNameAsc(String name, Pageable pageable);
+
     Page<Place> findByAddressNewContainingOrderByNameAsc(String addressNew, Pageable pageable);
 
     @Query("SELECT p FROM Place p ORDER BY p.createdAt LIMIT 5")
     List<Place> findTop5ByOrderByCreatedAtDESC();
+
+    <T> Page<T> findAllById(Long id, Pageable pageable, Class<T> type);
 }
