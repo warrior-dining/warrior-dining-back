0199491b28cb5f00aac35f9153f06095909db92c
JJAJJAKIM <csm9121@gmail.com>
2024-10-16 16:37:14 +0900

    [#46] [사용자] 마이페이지 - 즐겨찾기 리스트 데이터 게더링시 DTO사용

--- src/main/java/warriordiningback/api/user/service/UserReservationServiceImp.java
+++ src/main/java/warriordiningback/api/user/service/UserReservationServiceImp.java
@@ -2,9 +2,11 @@ package warriordiningback.api.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import warriordiningback.api.user.dto.UserReservationListResponse;
 import warriordiningback.domain.reservation.Reservation;
 import warriordiningback.domain.reservation.ReservationRepository;
 import warriordiningback.domain.user.User;
@@ -12,7 +14,9 @@ import warriordiningback.domain.user.UserRepository;
 import warriordiningback.exception.DiningApplicationException;
 import warriordiningback.exception.ErrorCode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -28,13 +32,19 @@ public class UserReservationServiceImp implements UserReservationService{
     @Override
     public Map<String, Object> myReservationList(String email, Pageable pageable) {
         Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("status", false);
         User user = userRepository.findByEmail(email).orElseThrow(() -> new DiningApplicationException(ErrorCode.USER_NOT_FOUND));
+        Page<Reservation> myReservations = reservationRepository.findAllByUserOrderByReservationDateDesc(user, pageable);
+        List<UserReservationListResponse> userReservationListResponse = new ArrayList<>();
 
-        Page<Reservation> myReservations;
-        myReservations = reservationRepository.findAllByUserOrderByReservationDateDesc(user, pageable);
+        for (Reservation row : myReservations.getContent()) {
+            UserReservationListResponse myReservation = new UserReservationListResponse(row);
+            userReservationListResponse.add(myReservation);
+        }
+        Page<UserReservationListResponse> results = new PageImpl<>(userReservationListResponse, myReservations.getPageable(), myReservations.getTotalElements());
 
         resultMap.put("status", true);
-        resultMap.put("results", myReservations);
+        resultMap.put("results", results);
         return resultMap;
     }
 
