5ea8499c61698ad3dc53728821bb5a6047977265
o-tao <dhtkdals369@gmail.com>
2024-10-17 16:02:34 +0900

    [#51] 불필요 어노테이션 제거, 테스트용 메서드 생성

--- src/main/java/warriordiningback/api/user/controller/UserController.java
+++ src/main/java/warriordiningback/api/user/controller/UserController.java
@@ -2,23 +2,26 @@ package warriordiningback.api.user.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import warriordiningback.api.user.dto.SignInRequest;
 import warriordiningback.api.user.dto.SignUpRequest;
 import warriordiningback.api.user.dto.UserResponse;
 import warriordiningback.api.user.service.UserService;
 import warriordiningback.domain.user.User;
+import warriordiningback.domain.user.UserRepository;
+import warriordiningback.exception.DiningApplicationException;
+import warriordiningback.exception.ErrorCode;
 import warriordiningback.token.response.TokenResponse;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @PostMapping("/signin")
     public TokenResponse signIn(@RequestBody @Valid SignInRequest signInRequest) {
@@ -37,8 +40,13 @@ public class UserController {
         return UserResponse.of(user);
     }
 
-    @PostMapping("/test")
-    public String test() {
-        return "test 성공";
+
+    // 테스트용 메서드입니다.
+    @PostMapping("/test/{id}")
+    public UserResponse test(@PathVariable("id") Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new DiningApplicationException(ErrorCode.USER_NOT_FOUND)
+        );
+        return UserResponse.of(user);
     }
 }
