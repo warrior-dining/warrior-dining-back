03b1f4112fd4a5bbb114426bf7aa5cf9b52a2b37
o-tao <dhtkdals369@gmail.com>
2024-10-21 17:08:38 +0900

    [#65] 불필요 어노테이션 제거

--- src/main/java/warriordiningback/api/user/controller/UserController.java
+++ src/main/java/warriordiningback/api/user/controller/UserController.java
@@ -2,7 +2,6 @@ package warriordiningback.api.user.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +12,6 @@ import warriordiningback.api.user.service.UserService;
 import warriordiningback.domain.user.User;
 import warriordiningback.token.response.TokenResponse;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
