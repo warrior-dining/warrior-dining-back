5d70a26eccb3d52ed7f6f6de74559aed32cd96a2
myin98 <dnjsdydpf@gmail.com>
2024-10-23 18:19:10 +0900

    [#83] 리뷰 목록 최신 리뷰 10개만 불러오기

---
+++ src/main/java/warriordiningback/api/restaurant/service/PlaceReviewService.java
@@ -0,0 +1,22 @@
+package warriordiningback.api.restaurant.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import warriordiningback.api.restaurant.dto.ReviewData;
+import warriordiningback.domain.review.ReviewRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    public List<ReviewData> findByReview() {
+        List<ReviewData> top10ReviewData = reviewRepository.findTop10ReviewData();
+        return top10ReviewData.stream().limit(10).toList();
+    }
+}
