1bd3f28f5807a52b264b803c78628d927f94ba1a
myin98 <dnjsdydpf@gmail.com>
2024-10-25 18:15:51 +0900

    [#103] User 컨트롤러 Valid 처리

--- src/main/java/warriordiningback/api/user/controller/UserInquiryController.java
+++ src/main/java/warriordiningback/api/user/controller/UserInquiryController.java
@@ -1,47 +1,40 @@
 package warriordiningback.api.user.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import warriordiningback.api.user.service.UserInquiryService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/member/inquiries")
 public class UserInquiryController {
-	
-	@Autowired
-	private UserInquiryService inquiryService;
-	
-	@GetMapping("/")
-	public Map<String, Object> inquiryList(@AuthenticationPrincipal UserDetails userDetails, Pageable pageable){
-		return inquiryService.inquiryList(userDetails, pageable);
-	}
 
-	@GetMapping("/{id:[0-9]+}")
-	public Map<String, Object> inquiryInfo(@PathVariable("id") Long id) {
-		return inquiryService.inquiryInfo(id);
-	}
-	
-	@PutMapping("/{id:[0-9]+}")
-	public Map<String, Object> inquiryInfoEdit(@PathVariable("id") Long id, @RequestBody Map<String, Object> inquiry){
-		return inquiryService.inquiryInfoEdit(id, inquiry);
-	}
-	
-	@PostMapping("/")
-	public Map<String, Object> newInquiry(@RequestBody Map<String, Object> newInquiries) {
-		return inquiryService.newInquiry(newInquiries);
-	}
-	
+    @Autowired
+    private UserInquiryService inquiryService;
+
+    @GetMapping("/")
+    public Map<String, Object> inquiryList(@AuthenticationPrincipal UserDetails userDetails, Pageable pageable) {
+        return inquiryService.inquiryList(userDetails, pageable);
+    }
+
+    @GetMapping("/{id:[0-9]+}")
+    public Map<String, Object> inquiryInfo(@PathVariable("id") Long id) {
+        return inquiryService.inquiryInfo(id);
+    }
+
+    @PutMapping("/{id:[0-9]+}")
+    public Map<String, Object> inquiryInfoEdit(@PathVariable("id") Long id,
+                                               @RequestBody Map<String, Object> inquiry) {
+        return inquiryService.inquiryInfoEdit(id, inquiry);
+    }
+
+    @PostMapping("/")
+    public Map<String, Object> newInquiry(@RequestBody Map<String, Object> newInquiries) {
+        return inquiryService.newInquiry(newInquiries);
+    }
+
 }
--- src/main/java/warriordiningback/api/user/controller/UserReviewController.java
+++ src/main/java/warriordiningback/api/user/controller/UserReviewController.java
@@ -1,69 +1,59 @@
 package warriordiningback.api.user.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import warriordiningback.api.user.service.UserReviewService;
-import warriordiningback.domain.reservation.Reservation;
 import warriordiningback.domain.review.ReviewRepository;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/member/reviews")
 public class UserReviewController {
 
-	@Autowired
-	private ReviewRepository reviewRepository;
-	
-	@Autowired
-	private UserReviewService userReviewService;
-	
-	@GetMapping("/reservation/{reservationId}")
-	public Map<String, Object> ReservationInfo(@PathVariable("reservationId") Long reservationId) {
-	    return userReviewService.ReservationInfo(reservationId);
-	}
-	
-	@PostMapping("/reservation/{reservationId}")
-	public Map<String, Object> createReview(@PathVariable("reservationId") Long reservationId, @RequestBody Map<String, Object> content) {
-		return userReviewService.createReview(reservationId, content);
-	}
-	
-	@GetMapping("/")
-	public Map<String, Object> myReviewList(@AuthenticationPrincipal UserDetails userDetails, Pageable pageable){
-		return userReviewService.myReviewList(userDetails, pageable);
-	}
-	
-	@PatchMapping("/{id:[0-9]+}")
-	public Map<String, Object> myReviewDelete(@PathVariable("id") Long id) {
-		return userReviewService.myReviewDelete(id);
-	}
-	
-	@GetMapping("/{id:[0-9]+}")
-	public Map<String, Object> myReviewInfo(@PathVariable("id") Long id){
-		return userReviewService.myReviewInfo(id);
-	}
-	
-	@PutMapping("/{id:[0-9]+}")
-	public Map<String, Object> myReviewInfoEdit(@PathVariable("id") Long id, @RequestBody Map<String, Object> content) {
-		return userReviewService.myReviewInfoEdit(id, content); 
-	}
-	
-	@DeleteMapping("/{id:[0-9]+}")
-	public Map<String, Object> myReviewUpdateStatus(@PathVariable("id") Long id){
-		return userReviewService.myReviewDelete(id);
-	}
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserReviewService userReviewService;
+
+    @GetMapping("/reservation/{reservationId}")
+    public Map<String, Object> ReservationInfo(@PathVariable("reservationId") Long reservationId) {
+        return userReviewService.ReservationInfo(reservationId);
+    }
+
+    @PostMapping("/reservation/{reservationId}")
+    public Map<String, Object> createReview(@PathVariable("reservationId") Long reservationId,
+                                            @RequestBody Map<String, Object> content) {
+        return userReviewService.createReview(reservationId, content);
+    }
+
+    @GetMapping("/")
+    public Map<String, Object> myReviewList(@AuthenticationPrincipal UserDetails userDetails, Pageable pageable) {
+        return userReviewService.myReviewList(userDetails, pageable);
+    }
+
+    @PatchMapping("/{id:[0-9]+}")
+    public Map<String, Object> myReviewDelete(@PathVariable("id") Long id) {
+        return userReviewService.myReviewDelete(id);
+    }
+
+    @GetMapping("/{id:[0-9]+}")
+    public Map<String, Object> myReviewInfo(@PathVariable("id") Long id) {
+        return userReviewService.myReviewInfo(id);
+    }
+
+    @PutMapping("/{id:[0-9]+}")
+    public Map<String, Object> myReviewInfoEdit(@PathVariable("id") Long id,
+                                                @RequestBody Map<String, Object> content) {
+        return userReviewService.myReviewInfoEdit(id, content);
+    }
+
+    @DeleteMapping("/{id:[0-9]+}")
+    public Map<String, Object> myReviewUpdateStatus(@PathVariable("id") Long id) {
+        return userReviewService.myReviewDelete(id);
+    }
 }
