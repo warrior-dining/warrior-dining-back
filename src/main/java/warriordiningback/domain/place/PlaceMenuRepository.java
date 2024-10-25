6209665bb459c4e17dea6ce2ea6e9b129d8a8a27
myin98 <dnjsdydpf@gmail.com>
2024-10-25 12:45:37 +0900

    [#92] 미사용 레포지토리 제거

--- src/main/java/warriordiningback/domain/place/PlaceMenuRepository.java
+++ src/main/java/warriordiningback/domain/place/PlaceMenuRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 public interface PlaceMenuRepository extends JpaRepository<PlaceMenu, Long> {
 
     List<PlaceMenu> findByPlaceId(Long placeId);
-    Long countByPlaceId(Long placeId);
 
 }
