3fe46f2400c53156c011df5867ed0274b57793c6
JJAJJAKIM <csm9121@gmail.com>
2024-10-28 16:09:32 +0900

    [#113] [문의사항] 사용자 문의내역 - 관리자 답변이 달린 경우 답변 내용까지 데이터 전달

--- src/main/java/warriordiningback/domain/inquiry/InquiriesAnswerRepository.java
+++ src/main/java/warriordiningback/domain/inquiry/InquiriesAnswerRepository.java
@@ -2,6 +2,9 @@ package warriordiningback.domain.inquiry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InquiriesAnswerRepository extends JpaRepository<InquiriesAnswer, Long> {
 
+    Optional<InquiriesAnswer> findByInquiry(Inquiry inquiry);
 }
